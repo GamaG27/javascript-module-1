b34f5fafc0e4af7f13ae0dd00502d00a5743d3f7
Damian <tricasaurio@gmail.com>
2022-03-05 15:08:01 +0100

    Exercise-k.js DONE

--- week-2/InClass/exercise-K.js
+++ week-2/InClass/exercise-K.js
@@ -1,1 +1,27 @@
-const students = [];
+const students = [
+  "Akram Almehanni",
+  "Alissa Nishihara",
+  "Asaad Ben Ahmed",
+  "burçak",
+  "Deniz Burcay",
+  "Ethan Carlsson",
+  "Gloria Kwofie",
+  "Isha Waheed",
+  "javi varela",
+  "Juan Amesty",
+  "Kostiantyn Borodach",
+  "Mehtap Parkinson",
+  "Rachid Essantouh",
+  "Shaminderjeet Kaur",
+  "usman sahi",
+  "Yuxuan Peng",
+];
+
+function student(number) {
+  for (let index = 0; index < students.length; index++) {
+    const name = students[index];
+    console.log("Hello " + name);
+  }
+}
+
+student();
